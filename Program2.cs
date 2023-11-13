e810a63ef906f2670d897218a30941b0be7caaea
Nostiia <150343865+Nostiia@users.noreply.github.com>
2023-11-13 14:47:46 +0200

    Delete Program2.cs

--- Program2.cs
+++
@@ -1,9 +0,0 @@
-﻿class Task
-{
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("How are you?");
-        string answer = Console.ReadLine();
-        Console.WriteLine("You are {0}", answer);
-    } 
-}
