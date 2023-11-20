84a3e998afc02d53c13451e8fc5bc1cbda3b0830
Nostiia <150343865+Nostiia@users.noreply.github.com>
2023-11-20 23:20:09 +0200

    Add files via upload

---
+++ Task4/Person.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonTask
+{
+    class Person
+    {
+        private string name;
+        private int birthYear;
+        public string Name
+        {
+            get { return name; }
+        }
+        public int BirthYear
+        {
+            get { return birthYear; }
+        }
+        public Person()
+        {
+            name = "Unknown";
+            int currentYear = DateTime.Now.Year;
+            birthYear = currentYear;
+        }
+        public Person(string name, int birthYear)
+        {
+            this.name = name;
+            this.birthYear = birthYear;
+        }
+        public static int Age(int birthYear)
+        {
+            int currentDate = DateTime.Now.Year;
+            int age = currentDate - birthYear;
+            return age;
+        }
+        public static Person Input()
+        {
+            Console.Write($"Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write($"Enter your year of birth: ");
+            int birthYear = int.Parse(Console.ReadLine());
+            Person person = new Person(name, birthYear);
+            return person;
+        }
+        public void ChangeName(string name)
+        {
+            name = "Very Young";
+            this.name = name;
+        }
+        public override string ToString()
+        {
+            return "Name: " + name + ", Year of Birth: " + birthYear;
+        }
+        public void Output()
+        {
+            Console.WriteLine(ToString());
+        }
+        public static bool operator ==(Person a, Person b)
+        {
+            return a.name.Equals(b.name);
+        }
+        public static bool operator !=(Person a, Person b)
+        {
+            return !(a.Equals(b));
+        }
+    }
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualBasic;
+
+namespace PersonTask
+{
+    class Program
+    {
+        static void Main()
+        {
+            Person[] people = new Person[6];
+            for (int i = 0; i < people.Length; i++)
+            {
+                people[i] = Person.Input();
+            }
+            int[] age = new int[people.Length];
+            for (int i = 0;i < people.Length; i++)
+            {
+                age[i] = Person.Age(people[i].BirthYear);
+                Console.WriteLine($"{people[i].Name} is {age[i]} years old.");
+            }
+            Console.WriteLine("Information about all persons:");
+            for(int i = 0; i < people.Length; i++)
+            {
+                if (age[i] < 16)
+                {
+                    people[i].ChangeName(people[i].Name);
+                }
+                Console.WriteLine(people[i].ToString());
+            }
+            Console.WriteLine("Persons with the same names:");
+            for (int i = 0; i < people.Length ; i++)
+            {
+                for (int j = i+1; j < people.Length; j++)
+                {
+                    if (people[i] == people[j])
+                    {
+                        Console.WriteLine(people[i].ToString());
+                        Console.WriteLine(people[j].ToString());
+                    }
+                }
+            }
+        }
+    }
+}
